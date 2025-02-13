d24c5d2b53d6023d283f7acdc7a545051f45f8f1
ImPowiz <77305867+ImPowiz@users.noreply.github.com>
2025-02-13 02:13:59 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,11 @@
 function selectOption(option) {
     // Check which option was clicked
     if (option === 'yes') {
-        function() {
+        // Flash rainbow colors
+        flashRainbowColors(function() {
             document.getElementById('question').style.display = 'none'; // Hide the question
             displayBearHeart(); // Display the cat-heart.gif
-        }
-            
+        });
     } else if (option === 'no') {
         // Change text on the "No" button to "You sure?"
         document.getElementById('no-button').innerText = 'You sure?'; 
@@ -21,6 +21,23 @@ function selectOption(option) {
         // If neither "Yes" nor "No" was clicked, show an alert message
         alert('Invalid option!');
     }
+}
+
+// Function to flash rainbow colors and then execute a callback function
+function flashRainbowColors(callback) {
+    var colors = [];
+    var i = 0;
+    var interval = setInterval(function() {
+        document.body.style.backgroundColor = colors[i];
+        i = (i + 1) % colors.length;
+    }, 200); // Change color every 200 milliseconds
+    setTimeout(function() {
+        clearInterval(interval);
+        document.body.style.backgroundColor = ''; // Reset background color
+        if (callback) {
+            callback();
+        }
+    }, 2000); // Flash colors for 2 seconds
 }
 
 // Function to display the cat.gif initially
